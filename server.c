6a308b4bd1ed0e7f01c2b12806c46595db734427
Yağız <savew.sh@proton.me>
2026-05-28 20:31:30 +0300

    refactor: modularize server architecture

--- server.c
+++ server.c
@@ -7,6 +7,7 @@
 #include <poll.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 #define BACKLOG 5
 #define MAX_CLIENTS 10 
@@ -14,9 +15,9 @@
 #define PORT 8080
 
 typedef struct {
-  int fd;
-  char username[32];
-  char id[6];
+    int fd;
+    char username[32];
+    char id[6];
 } ClientProfile;
 
 void randomId(char *str, int length) {
@@ -24,7 +25,7 @@ void randomId(char *str, int length) {
                      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                      "abcdefghijklmnopqrstuvwxyz";
                      
-    int charset_size = sizeof(charset) - 1; // Size is 62
+    int charset_size = sizeof(charset) - 1;
 
     for (int i = 0; i < length; i++) {
         int key = rand() % charset_size;
@@ -34,115 +35,150 @@ void randomId(char *str, int length) {
     str[length] = '\0';
 }
 
-int main() {
-  int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-  struct sockaddr_in server_addr;
+int init_server() {
+    int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_addr;
 
-  memset(&server_addr, 0, sizeof(server_addr));
+    memset(&server_addr, 0, sizeof(server_addr));
 
-  server_addr.sin_family = AF_INET;                 
-  server_addr.sin_port = htons(PORT);
-  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_family = AF_INET;                 
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  if (sock_fd < 0) {
-    log_err("Failed to open a socket.");
-    return 1;
-  }
-  log_info("Created a  socket.");
-  
-  if(bind(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-    log_err("Failed to bind socket.");
-    return 1;
-  } 
-  log_info("Bond socket.");
-  
-  if (listen(sock_fd, BACKLOG) < 0) {
-    log_err("Failed to listen socket.");
-    return 1;
-  }
-  log_info("Listening socket...");
-  log_info("Waiting connections...");
-
-  struct sockaddr_in client_addr;
-  socklen_t client_len = sizeof(client_addr);
-  struct pollfd fds[NFDS];
-  ClientProfile profiles[NFDS];
-  fds[0].fd = sock_fd;
-  fds[0].events = POLLIN;
-  fds[0].revents = 0;
-
-  nfds_t cnfds = 1;
-  
-  while(1) {
-
-    int activity = poll(fds, cnfds, -1);
-    if (activity < 0) {
-      log_err("Poll error.");
-      break;
+    if (sock_fd < 0) {
+        log_err("Failed to open a socket.");
+        return -1;
     }
+    log_info("Created a  socket.");
+  
+    if (bind(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        log_err("Failed to bind socket.");
+        close(sock_fd);
+        return -1;
+    } 
+    log_info("Bond socket.");
+  
+    if (listen(sock_fd, BACKLOG) < 0) {
+        log_err("Failed to listen socket.");
+        close(sock_fd);
+        return -1;
+    }
+    log_info("Listening socket...");
+    log_info("Waiting connections...");
 
-    if (fds[0].revents & POLLIN) {
-      int client_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &client_len);
-      if (client_fd < 0) {
+    return sock_fd;
+}
+
+void handle_new_connection(int sock_fd, struct pollfd *fds, ClientProfile *profiles, nfds_t *cnfds) {
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    int client_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &client_len);
+    if (client_fd < 0) {
         log_err("Failed to accept incoming client connection.");
-      }
-      log_info("Client connected with fd: %d", client_fd);
-      profiles[cnfds].fd = client_fd;
-      profiles[cnfds].username[0] = '\0';
-      randomId(profiles[cnfds].id, 5);
-      char welcome_msg[256];
-      snprintf(welcome_msg, sizeof(welcome_msg),
-               "\n==========================================\n"
-               "Welcome to the Save's Pocket Chat Server!\n"
-               "Your auto-generated ID is: [%s]\n"
-               "==========================================\n"
-               "Please enter your username: ", 
-               profiles[cnfds].id);
-      write(client_fd, welcome_msg, strlen(welcome_msg));
-      fds[cnfds].fd = client_fd;
-      fds[cnfds].events = POLLIN;
-      fds[cnfds].revents = 0;
-      cnfds++;
+        return;
     }
+    log_info("Client connected with fd: %d", client_fd);
 
+    profiles[*cnfds].fd = client_fd;
+    profiles[*cnfds].username[0] = '\0';
+    randomId(profiles[*cnfds].id, 5);
+
+    char welcome_msg[256];
+    snprintf(welcome_msg, sizeof(welcome_msg),
+             "\n==========================================\n"
+             "Welcome to the Save's Pocket Chat Server!\n"
+             "Your auto-generated ID is: [%s]\n"
+             "==========================================\n"
+             "Please enter your username: ", 
+             profiles[*cnfds].id);
+    write(client_fd, welcome_msg, strlen(welcome_msg));
+
+    fds[*cnfds].fd = client_fd;
+    fds[*cnfds].events = POLLIN;
+    fds[*cnfds].revents = 0;
+    (*cnfds)++;
+}
+
+int handle_client_data(int i, struct pollfd *fds, ClientProfile *profiles, nfds_t *cnfds) {
     char buffer[1024];
-    
-    for (int i = 1; i < cnfds; i++) {
-      
-      if (fds[i].revents & POLLIN) {
-        memset(buffer, 0, sizeof(buffer));
-        ssize_t bytes_read = read(fds[i].fd, buffer, sizeof(buffer) - 1);
-        if (bytes_read < 0) {
-          log_err("Failed to read message.");
-        }
-        else if (bytes_read == 0) {
-          log_warn("Client disconnected without sending anything.");
-          close(fds[i].fd);
-          fds[i] = fds[cnfds - 1];
-          cnfds--;
-          i--;
-        } else {
-            buffer[strcspn(buffer, "\r\n")] = '\0';
-            if (profiles[i].username[0] == '\0' ) {
-              strncpy(profiles[i].username, buffer, sizeof(profiles[i].username) - 1);
-              profiles[i].username[sizeof(profiles[i].username) - 1] = '\0';
-            } else {
-              char chat_msg[1500];
-              snprintf(chat_msg, sizeof(chat_msg), "[%s]: %s\n", profiles[i].username, buffer);
-              printf("#%s [%s]: %s\n", profiles[i].id, profiles[i].username, buffer);
-              for (int j = 1; j < cnfds; j++) {
+    memset(buffer, 0, sizeof(buffer));
+    ssize_t bytes_read = read(fds[i].fd, buffer, sizeof(buffer) - 1);
+
+    if (bytes_read < 0) {
+        log_err("Failed to read message.");
+        return 0;
+    }
+    else if (bytes_read == 0) {
+        log_warn("Client disconnected without sending anything.");
+        close(fds[i].fd);
+        fds[i] = fds[*cnfds - 1];
+        profiles[i] = profiles[*cnfds - 1];
+        (*cnfds)--;
+        return 1;
+    } 
+    else {
+        buffer[strcspn(buffer, "\r\n")] = '\0';
+
+        if (profiles[i].username[0] == '\0') {
+            strncpy(profiles[i].username, buffer, sizeof(profiles[i].username) - 1);
+            profiles[i].username[sizeof(profiles[i].username) - 1] = '\0';
+        } 
+        else {
+            char chat_msg[1500];
+            snprintf(chat_msg, sizeof(chat_msg), "[%s]: %s\n", profiles[i].username, buffer);
+            printf("#%s [%s]: %s\n", profiles[i].id, profiles[i].username, buffer);
+
+            for (int j = 1; j < *cnfds; j++) {
                 if (fds[j].fd != fds[i].fd && fds[j].fd > 0) {
-                  write(fds[j].fd, chat_msg, strlen(chat_msg));
+                    write(fds[j].fd, chat_msg, strlen(chat_msg));
                 }
-              }
             }
         }
-      }
+        return 0;
     }
-  }
+}
 
-  close(sock_fd);
-  log_info("Closed main server socket.");
+int main() {
+    srand(time(NULL));
 
-  return 0;
+    int sock_fd = init_server();
+    if (sock_fd < 0) {
+        return 1;
+    }
+
+    struct pollfd fds[NFDS];
+    ClientProfile profiles[NFDS];
+
+    fds[0].fd = sock_fd;
+    fds[0].events = POLLIN;
+    fds[0].revents = 0;
+
+    nfds_t cnfds = 1;
+  
+    while (1) {
+        int activity = poll(fds, cnfds, -1);
+        if (activity < 0) {
+            log_err("Poll error.");
+            break;
+        }
+
+        if (fds[0].revents & POLLIN) {
+            handle_new_connection(sock_fd, fds, profiles, &cnfds);
+        }
+
+        for (int i = 1; i < cnfds; i++) {
+            if (fds[i].revents & POLLIN) {
+                int disconnected = handle_client_data(i, fds, profiles, &cnfds);
+                if (disconnected) {
+                    i--;
+                }
+            }
+        }
+    }
+
+    close(sock_fd);
+    log_info("Closed main server socket.");
+
+    return 0;
 }
