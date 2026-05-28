237d460cc6d3c42f7b6736a4b90dbb68a1a597b9
Yağız <savew.sh@proton.me>
2026-05-28 18:17:59 +0300

    feat: update server architecture to async via poll

--- server.c
+++ server.c
@@ -4,8 +4,34 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <poll.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define BACKLOG 5
+#define MAX_CLIENTS 10 
+#define NFDS (MAX_CLIENTS + 1)
+
+typedef struct {
+  int fd;
+  char username[32];
+  char id[6];
+} ClientProfile;
+
+void randomId(char *str, int length) {
+    char charset[] = "0123456789"
+                     "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                     "abcdefghijklmnopqrstuvwxyz";
+                     
+    int charset_size = sizeof(charset) - 1; // Size is 62
+
+    for (int i = 0; i < length; i++) {
+        int key = rand() % charset_size;
+        str[i] = charset[key];
+    }
+    
+    str[length] = '\0';
+}
 
 int main() {
   int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -38,37 +64,80 @@ int main() {
 
   struct sockaddr_in client_addr;
   socklen_t client_len = sizeof(client_addr);
+  struct pollfd fds[NFDS];
+  ClientProfile profiles[NFDS];
+  fds[0].fd = sock_fd;
+  fds[0].events = POLLIN;
+  fds[0].revents = 0;
+
+  nfds_t cnfds = 1;
   
   while(1) {
-    int client_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &client_len);
-    if (client_fd < 0) {
-      log_err("Failed to accept client connection.");
-      return 1;
+
+    int activity = poll(fds, cnfds, -1);
+    if (activity < 0) {
+      log_err("Poll error.");
+      break;
     }
-    log_info("Client connected with fd: %d.", client_fd);
-    
-    log_info("Waiting client message.");
+
+    if (fds[0].revents & POLLIN) {
+      int client_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &client_len);
+      if (client_fd < 0) {
+        log_err("Failed to accept incoming client connection.");
+      }
+      log_info("Client connected with fd: %d", client_fd);
+      profiles[cnfds].fd = client_fd;
+      profiles[cnfds].username[0] = '\0';
+      randomId(profiles[cnfds].id, 5);
+      char welcome_msg[256];
+      snprintf(welcome_msg, sizeof(welcome_msg),
+               "\n==========================================\n"
+               "Welcome to the Save's Pocket Chat Server!\n"
+               "Your auto-generated ID is: [%s]\n"
+               "==========================================\n"
+               "Please enter your username: ", 
+               profiles[cnfds].id);
+      write(client_fd, welcome_msg, strlen(welcome_msg));
+      fds[cnfds].fd = client_fd;
+      fds[cnfds].events = POLLIN;
+      fds[cnfds].revents = 0;
+      cnfds++;
+    }
 
     char buffer[1024];
     
-    while(1) {
-      memset(buffer, 0, sizeof(buffer));
-    
-      ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
-
-      if (bytes_read < 0) {
-        log_err("Failed to read message.");
+    for (int i = 1; i < cnfds; i++) {
+      
+      if (fds[i].revents & POLLIN) {
+        memset(buffer, 0, sizeof(buffer));
+        ssize_t bytes_read = read(fds[i].fd, buffer, sizeof(buffer) - 1);
+        if (bytes_read < 0) {
+          log_err("Failed to read message.");
+        }
+        else if (bytes_read == 0) {
+          log_warn("Client disconnected without sending anything.");
+          close(fds[i].fd);
+          fds[i] = fds[cnfds - 1];
+          cnfds--;
+          i--;
+        } else {
+            buffer[strcspn(buffer, "\r\n")] = '\0';
+            if (profiles[i].username[0] == '\0' ) {
+              strncpy(profiles[i].username, buffer, sizeof(profiles[i].username) - 1);
+              profiles[i].username[sizeof(profiles[i].username) - 1] = '\0';
+            } else {
+              char chat_msg[1500];
+              snprintf(chat_msg, sizeof(chat_msg), "[%s]: %s\n", profiles[i].username, buffer);
+              printf("#%s [%s]: %s\n", profiles[i].id, profiles[i].username, buffer);
+              for (int j = 1; j < cnfds; j++) {
+                if (fds[j].fd != fds[i].fd && fds[j].fd > 0) {
+                  write(fds[j].fd, chat_msg, strlen(chat_msg));
+                }
+              }
+            }
+        }
       }
-      else if (bytes_read == 0) {
-        log_warn("Client disconnected without sending anything.");
-        break;
-      } else {
-        printf("[CLIENT]: %s\n", buffer);
-      }
-      write(client_fd, buffer, bytes_read);
-  }
-    close(client_fd);
-    log_info("Closed client connection.");
+    }
   }
 
   close(sock_fd);
